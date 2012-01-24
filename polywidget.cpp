a58d9d44f53658ca5e0bd82da0cee80f9fe26e7c
sergk <kovrov@gmail.com>
2012-01-24 17:43:34 +0200

    Shape rotation in cutscene command interpreter.

--- polywidget.cpp
+++ polywidget.cpp
@@ -14,8 +14,7 @@ class DPoly
 public:
     enum { WIDTH = 240, HEIGHT = 128 };
     DPoly()
-      : m_playing (false),
-        m_clear (false),
+      : m_clear (false),
         m_scale (1),
         m_cmd (Stream::fromBase64(dat_cmd)),
         m_pol (Stream::fromBase64(dat_pol)),
@@ -26,22 +25,13 @@ public:
     void start()
     {
         m_cmd.seek(2);
-        m_playing = true;
         // set default palette
         for (auto i = 0; i < 16; i++)
             m_palette_foreground.colors[i] = m_palette_backdrop.colors[i] = QColor(i, i, i);
     }
 
-    void pause()
-    {
-        m_playing = !m_playing;
-    }
-
     bool doTick(int &wait)
     {
-        if (!m_playing)
-            return false;
-
         if (m_start_new_shot) {
             m_start_new_shot = false;
             m_foreground_primitives.clear();
@@ -75,7 +65,7 @@ public:
                     x = m_cmd.next<qint16>();
                     y = m_cmd.next<qint16>();
                 }
-                addShape(shape_offset & 0x7FFF, QTransform().fromTranslate(x, y));
+                addShape(shape_offset & 0x7FFF, QTransform::fromTranslate(x, y));
             } break;
             case 4: {
                 const auto palette_index = m_cmd.next<quint8>();
@@ -94,41 +84,47 @@ public:
                 const auto pivot_x = m_cmd.next<quint8>();
                 const auto pivot_y = m_cmd.next<quint8>();
 
-                auto pos_matrix = QTransform().fromTranslate(x, y);
-                auto scaling_matrix = QTransform().fromScale(double(zoom)/512.0, double(zoom)/512.0);
-                auto pivot_origin = QTransform().fromTranslate(-pivot_x, -pivot_y);
-                auto restore_origin = QTransform().fromTranslate(pivot_x, pivot_y);
+                auto pivot_origin = QTransform::fromTranslate(-pivot_x, -pivot_y);
+                auto scaling_matrix = QTransform::fromScale(double(zoom)/512.0, double(zoom)/512.0);
+                auto restore_origin = QTransform::fromTranslate(pivot_x, pivot_y);
+                auto pos_matrix = QTransform::fromTranslate(x, y);
                 addShape(shape_info & 0x7FFF, pivot_origin * scaling_matrix * restore_origin * pos_matrix);
             } break;
             case 11: {
-                const auto shape_offset = m_cmd.next<quint16>();
+                const auto shape_info = m_cmd.next<quint16>();
                 qint16 x = 0, y = 0;
-                if (shape_offset & (1 << 15)) {
-                    y = m_cmd.next<qint16>();
+                if (shape_info & (1 << 15)) {
                     x = m_cmd.next<qint16>();
+                    y = m_cmd.next<qint16>();
                 }
                 quint16 zoom = 512;
-                if (shape_offset & (1 << 14)) {
+                if (shape_info & (1 << 14)) {
                     zoom += m_cmd.next<quint16>();  // expect to overflow
                 }
                 const auto pivot_x = m_cmd.next<quint8>();
                 const auto pivot_y = m_cmd.next<quint8>();
                 const auto r1 = m_cmd.next<quint16>();
                 quint16 r2 = 90;
-                if (shape_offset & (1 << 13)) {
+                if (shape_info & (1 << 13)) {
                     r2 = m_cmd.next<quint16>();
                 }
                 quint16 r3 = 180;
-                if (shape_offset & (1 << 12)) {
+                if (shape_info & (1 << 12)) {
                     r3 = m_cmd.next<quint16>();
                 }
-//                addShape(shape_offset & 0xFFF, y, x, double(zoom)/512.0, pivot_x, pivot_y, r1, r2, r3);
+                auto pivot_origin = QTransform::fromTranslate(-pivot_x, -pivot_y);
+                auto scaling_matrix = QTransform::fromScale(double(zoom)/512.0, double(zoom)/512.0);
+                auto restore_origin = QTransform::fromTranslate(pivot_x, pivot_y);
+                auto pos_matrix = QTransform::fromTranslate(x, y);
+                QTransform rotation_matrix;
+                rotation_matrix.rotate(-r1, Qt::ZAxis);
+                addShape(shape_info & 0xFFF, pivot_origin * rotation_matrix * scaling_matrix * restore_origin * pos_matrix);
             } break;
             case 12:
                 wait = 150;
                 return false;
             default:
-                m_playing = false;
+                Q_ASSERT (false);
             }
         }
         start();
@@ -137,7 +133,7 @@ public:
 
     void _drawNode(QPainter &painter, const Shape &shape, const Palete &palette)
     {
-        auto scale_matrix = QTransform().fromScale(m_scale, m_scale);
+        auto scale_matrix = QTransform::fromScale(m_scale, m_scale);
         foreach (const auto &primitive, shape.primitives) {
             const QColor &color = palette.colors[primitive.color_index];
             QPen pen(color);
@@ -166,8 +162,7 @@ public:
 private:
     void addShape(int offset, const QTransform &transform)
     {
-        Shape shape = { transform };
-        shape.primitives = m_pol.primitives(offset);
+        Shape shape = { transform, m_pol.primitives(offset)};
 
         if (m_clear) {
             m_backdrop_primitives.append(shape);
@@ -187,7 +182,6 @@ private:
         }
     }
 
-    bool m_playing;
     bool m_clear;
     Palete m_palette_foreground;
     Palete m_palette_backdrop;
